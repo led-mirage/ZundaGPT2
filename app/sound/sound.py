798b9a0f7922bfa239fbe419fd5512b0993788e2
led-mirage <139528700+led-mirage@users.noreply.github.com>
2025-07-27 13:43:01 +0900

    ラズパイ、Linux対応

--- app/sound/sound.py
+++ app/sound/sound.py
@@ -2,37 +2,51 @@
 #
 # WAVEデータ作成モジュール
 #
-# Copyright (c) 2024 led-mirage
+# Copyright (c) 2024-2025 led-mirage
 # このソースコードは MITライセンス の下でライセンスされています。
 # ライセンスの詳細については、このプロジェクトのLICENSEファイルを参照してください。
 
 import io
+import platform
+import os
+import tempfile
 import time
 import wave
 
-import pyaudio
 
 # 音声データを再生する
 def play_sound(wave_data: bytes):
-    wave_file = wave.open(io.BytesIO(wave_data), 'rb')
-    audio = pyaudio.PyAudio()
+    if platform.system() == "Windows":
+        import pyaudio # type: ignore
+        
+        wave_file = wave.open(io.BytesIO(wave_data), 'rb')
+        audio = pyaudio.PyAudio()
 
-    try:
-        format = audio.get_format_from_width(wave_file.getsampwidth())
+        try:
+            format = audio.get_format_from_width(wave_file.getsampwidth())
 
-        stream = audio.open(
-            format=format,
-            channels=wave_file.getnchannels(),
-            rate=wave_file.getframerate(),
-            output=True)
+            stream = audio.open(
+                format=format,
+                channels=wave_file.getnchannels(),
+                rate=wave_file.getframerate(),
+                output=True)
 
-        data = wave_file.readframes(1024)
-        while data != b'':
-            stream.write(data)
             data = wave_file.readframes(1024)
-        time.sleep(0.2)
-    finally:
-        stream.stop_stream()
-        stream.close()
-        audio.terminate()
-        wave_file.close()
+            while data != b'':
+                stream.write(data)
+                data = wave_file.readframes(1024)
+            time.sleep(0.2)
+        finally:
+            stream.stop_stream()
+            stream.close()
+            audio.terminate()
+            wave_file.close()
+    else:
+        with tempfile.NamedTemporaryFile(suffix='.wav', delete=False) as tmp:
+            tmp.write(wave_data)
+            tmp.flush()
+            tmpname = tmp.name
+        try:
+            os.system(f'aplay "{tmpname}"')
+        finally:
+            os.remove(tmpname)
