787c46f94a55f0a84691a475df8214faba2e5515
led-mirage <139528700+led-mirage@users.noreply.github.com>
2025-08-02 16:15:09 +0900

    ビルドバッチの更新

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
+if exist build rmdir /s /q build
 python update_version_yaml.py
 pyivf-make_version --source-format yaml --metadata-source version.yaml --outfile version.txt
 pyinstaller --onefile --noconsole --paths=./app --add-data "./app/html;html" --name ZundaGPT2 --icon assets/ZundaGPT2.ico --splash assets/ZundaGPT2_splash.png --version-file=version.txt app/main.py
--- build.sh
+++ build.sh
@@ -1,1 +1,3 @@
+set -e
+rm -rf build dist
 pyinstaller --onefile --noconsole --paths=./app --add-data "./app/html:html" --name ZundaGPT2 --splash assets/ZundaGPT2_splash.png app/main.py
