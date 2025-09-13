eaf5b25b1099683c8186b2413b3e27ede4b2596f
led-mirage <139528700+led-mirage@users.noreply.github.com>
2025-09-14 02:39:06 +0900

    ウィンドウサイズの保存・復元の修正

--- app/app_window.py
+++ app/app_window.py
@@ -6,11 +6,13 @@
 # このソースコードは MITライセンス の下でライセンスされています。
 # ライセンスの詳細については、このプロジェクトのLICENSEファイルを参照してください。
 
+import sys
 import threading
 
 import webview
 
 from config.app_config import AppConfig
+from utility.utils import get_screen_size
 
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
@@ -22,10 +24,12 @@ class AppWindow:
         self.app_config = app_config
         self.js: "JSCaller" = None
         self._resize_timer = None
+        self._is_minimized = False
+        self._is_maximized = False
 
     def set_raw_window(self, window: webview.Window):
         self._window = window
-        self._window.events.resized += self.on_resized
+        self.setup_window_events()
 
     def set_js_caller(self, js_caller: "JSCaller"):
         self.js = js_caller
@@ -45,7 +49,44 @@ class AppWindow:
     def open_cross_file_search_view(self):
         self._window.load_url("html/cross-file-search.html")
     
+    def setup_window_events(self):
+        self._window.events.minimized += self.on_minimized
+        self._window.events.maximized += self.on_maximized
+        self._window.events.restored += self.on_restored
+        self._window.events.resized += self.on_resized
+        self._window.events.shown += self.on_shown
+
+    def get_window_handle_for_windows(self):
+        if sys.platform == "win32":
+            return self._window.native.Handle
+        else:
+            return self._window.native
+
+    def on_shown(self):
+        if sys.platform == "win32":
+            width = self.app_config.system["window_width"]
+            height = self.app_config.system["window_height"]
+            screen_width, screen_height = get_screen_size(self.get_window_handle_for_windows())
+            adj_width = min(width, screen_width)
+            adj_height = min(height, screen_height)
+            self._window.resize(adj_width, adj_height)
+
+    def on_minimized(self):
+        self._is_minimized = True
+
+    def on_maximized(self):
+        self._is_maximized = True
+
+    def on_restored(self):
+        self._is_minimized = self._is_maximized = False
+        self._window.resizable = True
+        self._window.resize(1000,800)
+        print(self._window.resizable)
+
     def on_resized(self, width: int, height: int):
+        if self._is_minimized or self._is_maximized:
+            return
+
         # 既存タイマーをキャンセル
         if self._resize_timer and self._resize_timer.is_alive():
             self._resize_timer.cancel()
--- app/main.py
+++ app/main.py
@@ -42,7 +42,7 @@ class Application:
         self.app_config.load()
         VoicevoxAPI.server = self.app_config.tts["voicevox_server"]
         CoeiroinkApi.server = self.app_config.tts["coeiroink_server"]
-        width, height = self.adjust_size(
+        width, height = self.adjust_window_size(
             self.app_config.system["window_width"],
             self.app_config.system["window_height"]
         )
@@ -69,13 +69,14 @@ class Application:
         webview.start(gui=gui)
         #webview.start(gui=gui, debug=True) # 開発者ツールを表示する場合
 
-    # 渡されたウィンドウサイズをスクリーンサイズに収めて返す
-    def adjust_size(self, width: int, height: int) -> tuple[int, int]:
-        screen_width, screen_height = get_screen_size()
-        adj_width = min(width, screen_width)
-        adj_height = min(height, screen_height)
-        return adj_width, adj_height
-
+    def adjust_window_size(self, width: int, height: int):
+        if sys.platform != "win32": # WindowsはAppWindowのon_shownで処理する
+            screen_width, screen_height = get_screen_size()
+            width = min(width, screen_width)
+            height = min(height, screen_height)
+            return width, height
+        else:
+            return width, height
 
 if __name__ == '__main__':
     if getattr(sys, "frozen", False):
