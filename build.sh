a1ff5f8131d7b932b2a14bc78ec6a405e32988d4
led-mirage <139528700+led-mirage@users.noreply.github.com>
2025-10-05 10:04:08 +0900

    Linux用ビルドバッチ修正

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 set -e
 rm -rf build dist
-pyinstaller --onefile --noconsole --paths=./app --add-data "./app/html:html" --name ZundaGPT2 --splash assets/ZundaGPT2_splash.png app/main.py
+pyinstaller --onefile --noconsole --paths=./app --add-data "./app/html:html" --add-data "assets/ZundaGPT2-Icon64.png:assets" --name ZundaGPT2 --splash assets/ZundaGPT2_splash.png app/main.py
