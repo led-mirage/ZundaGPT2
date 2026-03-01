cb9521f8343b5664a468effa26c9755558e29ec9
led-mirage <139528700+led-mirage@users.noreply.github.com>
2026-03-01 14:52:11 +0900

    アプリバージョンの更新

--- app/const.py
+++ app/const.py
@@ -2,10 +2,10 @@
 #
 # アプリケーション定数
 #
-# Copyright (c) 2024-2025 led-mirage
+# Copyright (c) 2024-2026 led-mirage
 # このソースコードは MITライセンス の下でライセンスされています。
 # ライセンスの詳細については、このプロジェクトのLICENSEファイルを参照してください。
 
 APP_NAME = "ZundaGPT2"
-APP_VERSION = "1.39.1"
+APP_VERSION = "1.40.0"
 COPYRIGHT = "© 2024-2026 led-mirage"
